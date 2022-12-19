18bd016220f35b99aa6fe4224aecc2dd251fd06c
Dropmy <107980342+Dropmy@users.noreply.github.com>
2022-12-19 23:57:18 +0300

    Add files via upload

---
+++ MyWork/MyWork/CategoryModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyWork
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+        public string? Category { get; set; }
+        public int Discount { get; set; }
+    }
+
+    public class CategoryContext : DbContext
+    {
+        public DbSet<CategoryModel> Categories { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Привязка контекста к существующей бд
+            optionsBuilder.UseMySql("Server=MYSQL8001.site4now.net;Database=db_a8e04e_user090;Uid=a8e04e_user090;Pwd=qwerty5656;",
+            new MySqlServerVersion(new Version(5,0)));
+        }
+    }
+}
---
+++ MyWork/MyWork/Form1.cs
@@ -0,0 +1,119 @@
+﻿namespace MyWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DataGridViewDbLoad();
+        }
+
+        private void DataGridViewDbLoad()
+        {
+            using (CategoryContext db = new CategoryContext())
+            {
+                //Если нет ни одного элемента в бд
+                if (!db.Categories.Any())
+                {
+                    //Добавляем и сохраняем данные
+                    db.Categories.Add( new CategoryModel { Category = "Some Category", Discount = 13 } );
+                    db.SaveChanges();
+                }
+
+                //Отображаем текущее состояние бд 
+                dataGridView1.DataSource = db.Categories.ToList<CategoryModel>();
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string type = textBox2.Text;
+                int discount = Convert.ToInt32(textBox3.Text);
+
+                using (CategoryContext db = new CategoryContext())
+                {
+                    //Создаем модель для добавления в бд
+                    CategoryModel categoryToAdd = new CategoryModel { Category = type, Discount = discount };
+
+                    //Добавляем и сохраняем данные
+                    db.Categories.Add(categoryToAdd);
+                    db.SaveChanges();
+
+                    //Отображаем текущее состояние бд 
+                    dataGridView1.DataSource = db.Categories.ToList<CategoryModel>();
+                }
+            }
+            catch {
+                //Отображаем сообщение об ошибке
+                MessageBox.Show("Данные введены некоректно");
+                return;
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Попытка привести к нужному типу
+                int id = Convert.ToInt32(textBox1.Text);
+                string type = textBox2.Text;
+                int discount = Convert.ToInt32(textBox3.Text);
+
+                using (CategoryContext db = new CategoryContext())
+                {
+                    CategoryModel categoryToAdd = new CategoryModel { Category = type, Discount = discount, Id = id };
+                    try
+                    {
+                        //Пытаемся обновить данные
+                        db.Categories.Update(categoryToAdd);
+                        db.SaveChanges();
+
+                        //Отображаем текущее состояние бд 
+                        dataGridView1.DataSource = db.Categories.ToList<CategoryModel>();
+                    }
+                    catch
+                    {
+                        //Отображаем сообщение об ошибке
+                        MessageBox.Show("Невозможно обновить данных которых еще нет в БД");
+                    }
+                }
+            }
+            catch 
+            {
+                //Отображаем сообщение об ошибке
+                MessageBox.Show("Данные введены некоректно");
+                return;
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(textBox1.Text);
+
+                using (CategoryContext db = new CategoryContext())
+                {
+                    //Пытаемся найти категорию на удаление из бд
+                    CategoryModel? categoryToRemove = db.Categories.FirstOrDefault<CategoryModel?>(category => category.Id == id);
+                    if (categoryToRemove != null)
+                    {
+                        //Удаляем и сохраняем
+                        db.Categories.Remove(categoryToRemove);
+                        db.SaveChanges();
+
+                        //Отображаем текущее состояние бд 
+                        dataGridView1.DataSource = db.Categories.ToList<CategoryModel>();
+                    }
+                }
+            }
+            catch {
+                //Отображаем сообщение об ошибке
+                MessageBox.Show("Данные введены некоректно");
+                return;
+            }
+        }
+    }
+}
